3097f040dbd871142cf3f31a51e809709fef14a8
RodrigoBCastro <rodrigobragacastro@hotmail.com>
2024-01-19 10:16:08 -0300

    Excluding routes from file

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,4 @@
 import {createBrowserRouter, Navigate} from "react-router-dom";
-import Home from "./views/Home.jsx";
-import DefaultLayout from "./components/DefaultLayout";
-import GuestLayout from "./components/GuestLayout";
-import Login from "./views/Login";
-import NotFound from "./views/NotFound";
-import Signup from "./views/Signup";
-import Tasks from "./views/Tasks.jsx";
-import TaskForm from "./views/TaskForm.jsx";
 
 const router = createBrowserRouter([
 ])
