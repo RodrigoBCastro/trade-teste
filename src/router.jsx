70e513710b2822d0016c77660edb8db162deb6b9
RodrigoBCastro <rodrigobragacastro@hotmail.com>
2024-01-19 10:12:54 -0300

    Some changes to use React

---
+++ src/router.jsx
@@ -0,0 +1,14 @@
+import {createBrowserRouter, Navigate} from "react-router-dom";
+import Home from "./views/Home.jsx";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Login from "./views/Login";
+import NotFound from "./views/NotFound";
+import Signup from "./views/Signup";
+import Tasks from "./views/Tasks.jsx";
+import TaskForm from "./views/TaskForm.jsx";
+
+const router = createBrowserRouter([
+])
+
+export default router;
